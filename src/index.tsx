15cf36af24fd0f68a92bbaac50aea483175f1974
Jakub Zawadzki <decay@memleak.pl>
2022-05-05 22:34:00 +0200

    One more time

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.PUBLIC_URL
+axios.defaults.baseURL = `${process.env.PUBLIC_URL}/api`
 
 ReactDOM.render(
     <React.StrictMode>
