4cc63bf4e897904d84d6ef37368cce568a13373c
Decay <jakub.zawadzki3@gmail.com>
2022-06-29 22:06:37 +0200

    Fix login url

--- src/Components/Base/Login.tsx
+++ src/Components/Base/Login.tsx
@@ -1,7 +1,7 @@
 export default function LoginPage() {
 
   return (
-    <a href='http://localhost:8080/api/auth' rel='noopener noreferrer'>
+    <a href='http://nba-nginx.herokuapp.com/api/auth' rel='noopener noreferrer'>
       Logowanie za pomocą Google OAuth
     </a>
   );
